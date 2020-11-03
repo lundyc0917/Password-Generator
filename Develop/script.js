8c69f0f895062cb31a6fde20df9b50eb175d35b2
lundyc0917 <lundyc0917@gmail.com>
2020-11-03 17:36:40 -0500

    Updated .js code

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,26 @@ function writePassword() {
 
   passwordText.value = password;
 
+  
+
 }
+
+function generatePasword () {
+    // Length - Enter an Integer between 8 - 128 Characters
+  var passwordLength = prompt ("Enter the number of characters you would like.  It must be more than 8 and less than 128.");
+
+    // Char Types - select: lowercase, uppercase, numeric, and/or special chars
+  var lowerCase = confirm ("Do you want lowercase characters in your password?");
+
+  var upperCase = confirm ("Do you want uppercase characters in your password?");
+
+  var numeric = confirm ("Do you want numbers in your password?");
+
+  var specialChar = confirm ("Do you want special characters in your password?");
+
+}
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
