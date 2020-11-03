42b13720c3285868ba7a603fc7bcecc8f7f61675
lundyc0917 <lundyc0917@gmail.com>
2020-11-03 18:36:05 -0500

    Added user prompts to .js.  Broken code

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Special Char for the function
+const specialChar = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,10 +15,22 @@ function writePassword() {
 
 }
 
-function generatePasword () {
+function generatePassword () {
     // Length - Enter an Integer between 8 - 128 Characters
   var passwordLength = prompt ("Enter the number of characters you would like.  It must be more than 8 and less than 128.");
 
+  if (isNaN(passwordLength)===true){12
+    alert("Must be more than 8 characters.");
+    return;
+  }
+  if (passwordLength < 8){
+    alart("Must be more than 8 characters.");
+    return;
+  }
+  if (passwordLength > 128){
+    alert("Must be less than 128 characters.");
+    return;
+  }
     // Char Types - select: lowercase, uppercase, numeric, and/or special chars
   var lowerCase = confirm ("Do you want lowercase characters in your password?");
 
@@ -25,7 +40,49 @@ function generatePasword () {
 
   var specialChar = confirm ("Do you want special characters in your password?");
 
+  if (
+    lowerCase === false&&
+    upperCase === false&&
+    numeric === false&&
+    specialChar === false
+  ){
+    alert ("You must select one type of character.");
+    return;
+  }
+  var userInput = {passwordLength:passwordLength, lowerCase:lowerCase, upperCase:upperCase, specialChar:specialChar};
+  return userInput;
 }
+// End of 
+
+
+
+if (lowerCase === true) {
+  minLowerCase = functionArray[1];
+  miniChar++;
+
+}
+
+if (upperCase === true) {
+  minUpperCase = functionArray[2];
+  minChar++;
+
+}
+
+if (numeric === true) {
+  minNumeric = functionArray[0];
+  minChar++;
+
+}
+
+if (specialChar === true) {
+  minSpecialChar = functionArray[3];
+  minChar++;
+}
+
+
+
+
+return randomPasswordGenerated;
 
 
 
